4919d5f6cb5a8c340c77f47116c03464096474f2
Janek Winkler <janekx21@gmail.com>
2024-07-01 12:36:36 +0200

    test: added a test

--- src/main.rs
+++ src/main.rs
@@ -256,9 +256,6 @@ async fn main() {
         error!("paniced with {}", info);
     }));
 
-    let stdin = tokio::io::stdin();
-    let stdout = tokio::io::stdout();
-
     let mut parser = Parser::new();
     parser.set_language(*LANGUAGE).unwrap();
 
@@ -268,6 +265,9 @@ async fn main() {
         document_map: DashMap::new(),
         position_encoding: PositionEncodingKind::UTF16.into(),
     });
+
+    let stdin = tokio::io::stdin();
+    let stdout = tokio::io::stdout();
     Server::new(stdin, stdout, socket).serve(service).await;
 }
 
@@ -1004,76 +1004,6 @@ fn node_text<'a>(node: &Node, rope: &'a Rope) -> ropey::RopeSlice<'a> {
     rope.byte_slice(node.start_byte()..node.end_byte())
 }
 
-#[test]
-fn test_parse() {
-    let mut parser = Parser::new();
-    parser.set_language(*LANGUAGE).unwrap();
-
-    let source_code = "Ontology: Foobar";
-    let tree = parser.parse(source_code, None).unwrap();
-
-    assert_eq!(
-        tree.root_node().to_sexp(),
-        "(source_file (ontology (ontology_iri (simple_iri))))"
-    );
-}
-
-#[test]
-fn test_parse_datatype() {
-    let mut parser = Parser::new();
-    parser.set_language(*LANGUAGE).unwrap();
-
-    let source_code = "Ontology: o\nDatatype: d";
-    let tree = parser.parse(source_code, None).unwrap();
-
-    assert_eq!(
-        tree.root_node().to_sexp(),
-        "(source_file (ontology (ontology_iri (simple_iri)) (datatype_frame (datatype_iri (simple_iri)))))"
-    );
-}
-
-#[test]
-fn test_class_annotation_query() {
-    use tree_sitter::QueryMatch;
-
-    let mut parser = Parser::new();
-    parser.set_language(*LANGUAGE).unwrap();
-
-    let source_code = "
-Ontology: <http://foo.bar>
-    Class: <http://foo.bar/0>
-        Annotations:
-            rdfs:label \"Fizz\"
-";
-    let tree = parser.parse(source_code, None).unwrap();
-
-    let query_source = "
-                            (class_frame (class_iri (full_iri)@iri)\
-                                (annotation\
-                                    (annotation_property_iri (abbreviated_iri)@abbriviated-iri)\
-                                    (string_literal_no_language)@literal))";
-
-    let class_frame_query = Query::new(*LANGUAGE, query_source).unwrap();
-    let root = tree.root_node();
-    let mut query_cursor = QueryCursor::new();
-    let bytes: &[u8] = source_code.as_bytes();
-    let matches = query_cursor
-        .matches(&class_frame_query, root, bytes)
-        .collect::<Vec<QueryMatch>>();
-
-    println!("{}", root.to_sexp());
-
-    assert_eq!(matches.len(), 1);
-
-    assert_eq!(
-        matches[0].captures[1]
-            .node
-            .utf8_text(bytes)
-            .expect("valid utf-8"),
-        "rdfs:label"
-    );
-}
-
 fn timeit<F: FnMut() -> T, T>(name: &str, mut f: F) -> T {
     use std::time::Instant;
     let start = Instant::now();
@@ -1214,4 +1144,162 @@ fn node_type_to_keyword(_type: &str) -> Option<String> {
         "has_key" => Some("HasKey:".to_string()),
         _ => None,
     }
+}
+
+#[test]
+fn test_parse() {
+    let mut parser = Parser::new();
+    parser.set_language(*LANGUAGE).unwrap();
+
+    let source_code = "Ontology: Foobar";
+    let tree = parser.parse(source_code, None).unwrap();
+
+    assert_eq!(
+        tree.root_node().to_sexp(),
+        "(source_file (ontology (ontology_iri (simple_iri))))"
+    );
+}
+
+#[test]
+fn test_parse_datatype() {
+    let mut parser = Parser::new();
+    parser.set_language(*LANGUAGE).unwrap();
+
+    let source_code = "Ontology: o\nDatatype: d";
+    let tree = parser.parse(source_code, None).unwrap();
+
+    assert_eq!(
+        tree.root_node().to_sexp(),
+        "(source_file (ontology (ontology_iri (simple_iri)) (datatype_frame (datatype_iri (simple_iri)))))"
+    );
+}
+
+#[test]
+fn test_class_annotation_query() {
+    use tree_sitter::QueryMatch;
+
+    let mut parser = Parser::new();
+    parser.set_language(*LANGUAGE).unwrap();
+
+    let source_code = "
+Ontology: <http://foo.bar>
+    Class: <http://foo.bar/0>
+        Annotations:
+            rdfs:label \"Fizz\"
+";
+    let tree = parser.parse(source_code, None).unwrap();
+
+    let query_source = "
+                            (class_frame (class_iri (full_iri)@iri)\
+                                (annotation\
+                                    (annotation_property_iri (abbreviated_iri)@abbriviated-iri)\
+                                    (string_literal_no_language)@literal))";
+
+    let class_frame_query = Query::new(*LANGUAGE, query_source).unwrap();
+    let root = tree.root_node();
+    let mut query_cursor = QueryCursor::new();
+    let bytes: &[u8] = source_code.as_bytes();
+    let matches = query_cursor
+        .matches(&class_frame_query, root, bytes)
+        .collect::<Vec<QueryMatch>>();
+
+    println!("{}", root.to_sexp());
+
+    assert_eq!(matches.len(), 1);
+
+    assert_eq!(
+        matches[0].captures[1]
+            .node
+            .utf8_text(bytes)
+            .expect("valid utf-8"),
+        "rdfs:label"
+    );
+}
+
+/// This tests if the "did_change" feature works on the lsp. It takes the document DEF and adds two changes resolving in ABCDEFGHI.
+#[tokio::test]
+async fn test_language_server_did_change() {
+    let mut parser = Parser::new();
+    parser.set_language(*LANGUAGE).unwrap();
+
+    let (service, _) = LspService::new(|client| Backend {
+        client,
+        parser: Mutex::new(parser),
+        document_map: DashMap::new(),
+        position_encoding: PositionEncodingKind::UTF16.into(),
+    });
+
+    let _ = service
+        .inner()
+        .initialize(InitializeParams {
+            ..Default::default()
+        })
+        .await;
+
+    let url = Url::parse("file://foo.omn").expect("valid url");
+    service
+        .inner()
+        .did_open(DidOpenTextDocumentParams {
+            text_document: TextDocumentItem {
+                uri: url.clone(),
+                language_id: "owl2md".to_string(),
+                version: 0,
+                text: "DEF".to_string(),
+            },
+        })
+        .await;
+    service
+        .inner()
+        .did_change(DidChangeTextDocumentParams {
+            text_document: VersionedTextDocumentIdentifier {
+                uri: url.clone(),
+                version: 2,
+            },
+            content_changes: vec![
+                TextDocumentContentChangeEvent {
+                    range: Some(
+                        Range {
+                            start: (Position {
+                                line: 0,
+                                character: 0,
+                            }),
+                            end: Position {
+                                line: 0,
+                                character: 0,
+                            },
+                        }
+                        .into(),
+                    ),
+                    range_length: None,
+                    text: "ABC".to_string(),
+                },
+                TextDocumentContentChangeEvent {
+                    range: Some(
+                        Range {
+                            start: (Position {
+                                line: 0,
+                                character: 6,
+                            }),
+                            end: Position {
+                                line: 0,
+                                character: 6,
+                            },
+                        }
+                        .into(),
+                    ),
+                    range_length: None,
+                    text: "GHI".to_string(),
+                },
+            ],
+        })
+        .await;
+
+    let doc = service
+        .inner()
+        .document_map
+        .get(&url.clone())
+        .expect("found the document");
+    let doc_content = doc.rope.to_string();
+
+    assert_eq!(doc_content, "ABCDEFGHI");
 }
