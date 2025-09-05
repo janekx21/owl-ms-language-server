4182f30649bd4f18044c77e10af4e4db2e2cc81d
Janek Winkler <janekx21@gmail.de>
2025-09-05 17:58:30 +0200

    fix: change used range indices to bytes instead of chars

--- crates/owl-ms-language-server/src/workspace.rs
+++ crates/owl-ms-language-server/src/workspace.rs
@@ -571,12 +571,7 @@ impl InternalDocument {
 
                     // rope replace
                     timeit("rope operations", || {
-                        if old_end_char < start_char {
-                            error!(
-                                "Invalid rope remove operation range. {start_char}..{old_end_char}"
-                            );
-                        }
-                        // TODO this panics now while formatting oeo physical :<
+                        // TODO #24 change to try_* functions
                         self.rope.remove(start_char..old_end_char);
                         self.rope.insert(start_char, &change.text);
                     });
