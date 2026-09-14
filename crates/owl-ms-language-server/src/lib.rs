0f0b71a607ecacc68e627f3ab18eec9aa09a7983
Janek Winkler <janekx21@gmail.de>
2026-09-14 11:42:39 +0200

    add file path goto definition

--- crates/owl-ms-language-server/src/lib.rs
+++ crates/owl-ms-language-server/src/lib.rs
@@ -675,7 +675,10 @@ impl LanguageServer for Backend {
             if *is_import {
                 let url = Url::parse(iri.as_str()).map_err(|_| Error::InvalidUrl(url.clone()))?;
                 // This does not work for external documents from prefixes
-                let path = workspace.url_to_path_with_catalog(&url);
+
+                let path = workspace
+                    .url_to_path_with_catalog(&url)
+                    .or_else(|| url.to_file_path().ok());
                 if let Some(path) = path {
                     return Ok(Some(single_path_response(&path)));
                 }
